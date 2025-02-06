b6bd652692ae2ac82bdd2b4ceca0bd97bc5dd530
CWmain <cameronwhite2478@gmail.com>
2025-02-07 02:14:42 +1100

    Added image functionality to the card, have cards return to a set home location upon release which may be controlled by other nodes

--- Objects/Cards/base_card.gd
+++ Objects/Cards/base_card.gd
@@ -1,11 +1,46 @@
 extends Node2D
 
 @export var data: CustomCard
+@export var SPEED: float = 20.0
+
 @onready var label = $Label
 @onready var icon = $Icon
+@onready var color_rect = $ColorRect
+
+var canGrab: bool = true
+var isGrabbing: bool = false
+
+var goHome: bool = false
+var home: Vector2 = Vector2(250,250)
 
 func _ready():
 	assert(data != null)
 	
-	icon.texture = load(data.image)
 	label.text = str(data.small) + " / " + str(data.large)
+	icon.texture = load(data.image)
+
+func _process(delta):
+	if Input.is_action_pressed("Grab") and (canGrab or isGrabbing):
+		position = get_global_mouse_position()
+		isGrabbing = true
+		goHome = false
+		
+	if Input.is_action_just_released("Grab"):
+		isGrabbing = false
+		goHome = true
+	
+	# Move card to home location
+	if goHome:
+		position += (home - position).normalized()*SPEED
+	if (home - position).length() < SPEED:
+		position = home
+		goHome = false
+
+func _on_color_rect_mouse_entered():
+	color_rect.color = Color(1,1,1,1)
+	canGrab = true
+
+
+func _on_color_rect_mouse_exited():
+	color_rect.color = Color(0,0,0,1)
+	canGrab = false
