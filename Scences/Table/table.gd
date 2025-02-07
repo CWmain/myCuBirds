1e40aa16f5f13007b19a25c01a6b4f95aef2f73e
CWmain <cameronwhite2478@gmail.com>
2025-02-07 20:02:40 +1100

    Imported my concept image. Added a button which increase a score, and display them different depening on who did it

--- Scences/Table/table.gd
+++ Scences/Table/table.gd
@@ -3,6 +3,11 @@ extends Node2D
 @onready var host_hand = $HostHand
 @onready var client_hand = $ClientHand
 
+@onready var my_points = $MyPoints
+@onready var their_points = $TheirPoints
+
+var points: int = 0
+
 func _ready():
 	multiplayer.peer_connected.connect(_on_connected_to_server)
 	if multiplayer.is_server():
@@ -18,4 +23,13 @@ func _process(delta):
 func _on_connected_to_server(id: int):
 	client_hand.set_multiplayer_authority(id)
 	
+	
+func _on_button_pressed():
+	points += 1
+	my_points.text = str(points)
+	updateTheirPoints.rpc(points)
+
+@rpc("any_peer", "call_remote")
+func updateTheirPoints(p: int):
+	their_points.text = str(p)
 	
