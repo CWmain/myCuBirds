1e31e8a0b79f9cd67be891d3166ba05af665596b
CWmain <cameronwhite2478@gmail.com>
2025-02-08 18:58:51 +1100

    Have cards being able to be added through hand by passing the resource for the card

--- Objects/Hand/hand.gd
+++ Objects/Hand/hand.gd
@@ -3,9 +3,27 @@ extends Node2D
 var id: int = 0
 @export var newHome: Vector2 = Vector2.ZERO
 @export var tname: String = "default"
+@export var cardScale: float = 0.2
+@onready var container = $Container
 
+var baseCard = preload("res://Objects/Cards/base_card.tscn")
+var catCard = preload("res://Objects/Cards/Cat/cat.tres")
+var flamingoCard = preload("res://Objects/Cards/Flamingo/Flamingo.tres")
+var owlCard = preload("res://Objects/Cards/Owl/Owl.tres")
 func _ready():
-	pass
+	addCard(flamingoCard)
+	addCard(flamingoCard)
+	addCard(catCard)
+	addCard(owlCard)
 
 func _process(_delta):
 	pass
+	
+## Adds a card into your hand based on a given resource
+func addCard(toAdd: Resource):
+	var cc = Control.new()
+	var newCard = baseCard.instantiate()
+	newCard.data = toAdd
+	newCard.scale = Vector2(cardScale,cardScale)
+	cc.add_child(newCard)
+	container.add_child(cc)
