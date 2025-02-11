9a578d8e904edc75207197578a36b8db55c611a5
CWmain <cameronwhite2478@gmail.com>
2025-02-11 21:21:49 +1100

    Have flyhome update total points, need to rework with a score tracker. As its not a simple as counting a total points

--- Objects/FlyHome/fly_home.gd
+++ Objects/FlyHome/fly_home.gd
@@ -14,19 +14,29 @@ func _process(delta):
 	# A given player card is dragged and dropped onto the detector
 	if cardHeld != null and Input.is_action_just_released("Grab"):
 		# Check the count of the given id and if that is enough birds to fly home
-		
-		
-		# Free all like cards 
-		for c in get_tree().get_nodes_in_group("card"):
-			if c.data.id == cardHeld.data.id:
-				# Free the parent control and than itself
-				myHand.removeCard(c)
+		var pointsEarned:int = scoreFromBird(cardHeld)
+		if (pointsEarned > 0):
+			# Free all like cards 
+			for c in get_tree().get_nodes_in_group("card"):
+				if c.data.id == cardHeld.data.id:
+					# Free the parent control and than itself
+					myHand.removeCard(c)
+		#TODO: Current setting score to 1 or 2, need a score tracking 
+		#		object to highlight which birds are flown home and how many points
+		get_parent().updatePoints.rpc(multiplayer.get_unique_id(), pointsEarned)
 
 
 ## Get the score associated with the amount of birds flown home
 ## Returns 0 on fail, 1 on min met and 2 on max met
-func scoreFromBird(c: Object) -> int:
-	
+func scoreFromBird(scoreCard: Object) -> int:
+	var cardCount: int = 0
+	for c in Global.cardsInHand:
+		if c.data.id == scoreCard.data.id:
+			cardCount += 1
+	if cardCount >= scoreCard.data.large:
+		return 2
+	if cardCount >= scoreCard.data.small:
+		return 1
 	return 0
 
 func _on_area_2d_area_entered(area):
