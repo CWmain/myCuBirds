02392e56579483a9c7c1136ad4d303c24c791a77
CWmain <cameronwhite2478@gmail.com>
2025-02-12 20:44:19 +1100

    Have left and right side detectors add cards to collection row

--- Objects/CollectionRow/collection_row.gd
+++ Objects/CollectionRow/collection_row.gd
@@ -19,30 +19,52 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if leftCard != null and Input.is_action_just_released("Grab"):
-
+		# Use curCard as when the original is reparent is triggers 
+		# the area exit, resetting it
+		var curCard = leftCard
+		
 		var i: int = 0
-		for c in Global.cardsInHand:
+		while (i < Global.cardsInHand.size()):
+			var c = Global.cardsInHand[i]
 
-			if c.data.id == leftCard.data.id:
-				var baseControl = Control.new()
+			if c.data.id == curCard.data.id:
+				# Remove from cardsInHand array
 				Global.cardsInHand.remove_at(i)
-				c.reparent(baseControl)
-				row.add_child(baseControl)
-				row.move_child(baseControl,1)
+
+				# Reparent cards control parent to CollectionRow
+				c.get_parent().reparent(row)
+				row.move_child(c.get_parent(),1)
 				c.position = Vector2(0,0)
+				
+				# Since we remove an element from the array, subtract 1 from index
+				i -= 1
 			i += 1
+		# Ensure leftCard is reset
+		leftCard = null
 				
 	if rightCard != null and Input.is_action_just_released("Grab"):
+		# Use curCard as when the original is reparent is triggers 
+		# the area exit, resetting it
+		var curCard = rightCard
+		
 		var i: int = 0
-		for c in Global.cardsInHand:
-			if c.data.id == rightCard.data.id:
-				var baseControl = Control.new()
+		while (i < Global.cardsInHand.size()):
+			var c = Global.cardsInHand[i]
+
+			if c.data.id == curCard.data.id:
+				# Remove from cardsInHand array
 				Global.cardsInHand.remove_at(i)
-				c.reparent(baseControl)
-				row.add_child(baseControl)
-				row.move_child(baseControl,-2)
+
+				# Reparent cards control parent to CollectionRow
+				c.get_parent().reparent(row)
+				row.move_child(c.get_parent(),-2)
 				c.position = Vector2(0,0)
+				
+				# Since we remove an element from the array, subtract 1 from index
+				i -= 1
 			i += 1
+		# Ensure rightCard is reset
+		rightCard = null
 
 
 func _on_left_area_2d_area_entered(_area):
