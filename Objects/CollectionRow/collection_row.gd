25f1072d98c35409acc7b3aefbc0ffbc8b31aaa6
CWmain <cameronwhite2478@gmail.com>
2025-02-12 23:32:08 +1100

    Cards in CollectionRow now squish like cards in hand

--- Objects/CollectionRow/collection_row.gd
+++ Objects/CollectionRow/collection_row.gd
@@ -6,6 +6,10 @@ extends Control
 # When placed on the left scan left to right, at the first instance of the same 
 # card, move all cards into the players hand (Oppisite for right side)
 # If non of the same card is found, allow the player to draw
+
+@export var MAX_SEP: int = 128
+@export var MIN_SEP: int = 0
+
 @onready var row = $Row
 
 var leftCard: Object = null
@@ -18,6 +22,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	var windowSize: Vector2 = Vector2(DisplayServer.window_get_size())
+	var currentSeparation: int = row.get_theme_constant("separation")
+	# The added Vector is due to cards being centered, so this gives some extra magin
+	if (row.size+Vector2(256,256) > windowSize and currentSeparation > MIN_SEP):
+		row.add_theme_constant_override("separation", currentSeparation-1)
+		currentSeparation -= 1
+	if (row.size+Vector2(256,256) < windowSize and currentSeparation < MAX_SEP):
+		row.add_theme_constant_override("separation", currentSeparation+1)
+	
 	if leftCard != null and Input.is_action_just_released("Grab"):
 		# Use curCard as when the original is reparent is triggers 
 		# the area exit, resetting it
@@ -28,14 +41,13 @@ func _process(_delta):
 			var c = Global.cardsInHand[i]
 
 			if c.data.id == curCard.data.id:
+				c.goHome = false
 				# Remove from cardsInHand array
 				Global.cardsInHand.remove_at(i)
-
 				# Reparent cards control parent to CollectionRow
 				c.get_parent().reparent(row)
 				row.move_child(c.get_parent(),1)
 				c.position = Vector2(0,0)
-
 				# Since we remove an element from the array, subtract 1 from index
 				i -= 1
 			i += 1
