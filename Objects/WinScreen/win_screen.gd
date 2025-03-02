36e120ea5d47908957b0b3aaa2af80ec1672b3bd
CWmain <cameronwhite2478@gmail.com>
2025-03-02 14:08:31 +1100

    Added buttons to end game screen with emitts set up for buttons

--- Objects/WinScreen/win_screen.gd
+++ Objects/WinScreen/win_screen.gd
@@ -2,5 +2,16 @@ extends Control
 
 @onready var winner_name = $VBoxContainer/WinnerName
 
+signal rematch
+signal main_menu
+
 func assignWinner(winner: String):
 	winner_name.text = winner
+
+
+func _on_rematch_pressed():
+	rematch.emit()
+
+
+func _on_main_menu_pressed():
+	main_menu.emit()
